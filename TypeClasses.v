a8e5e838a73030355cf6f4f20d322abe9f509634
beta <beta@mpi-sws.org>
2020-12-13 12:42:17 -0300

    Typeclasses now compiles

--- TypeClasses.v
+++ TypeClasses.v
@@ -6,14 +6,14 @@ Section with_var.
   Context {var : Type}.
 
   Class reify_of (term : nat) (rterm : @expr var) := {}.
-  Global Instance reify_NatMul `{reify_of x rx, reify_of y ry}
+  Program Global Instance reify_NatMul `{reify_of x rx, reify_of y ry}
     : reify_of (x * y) (rx * ry).
-  Global Instance reify_LetIn `{reify_of x rx}
+  Program Global Instance reify_LetIn `{reify_of x rx}
          `{forall y ry, reify_of y (Var ry) -> reify_of (f y) (rf ry)}
     : reify_of (dlet y := x in f y) (elet ry := rx in rf ry).
-  Global Instance reify_S `{reify_of x rx}
+  Program Global Instance reify_S `{reify_of x rx}
     : reify_of (S x) (NatS rx).
-  Global Instance reify_O
+  Program Global Instance reify_O
     : reify_of O NatO.
 End with_var.
 
