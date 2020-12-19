a3635cf54e9fc87ea64a6a95364d3984058c6508
beta <beta@mpi-sws.org>
2020-12-18 22:05:07 -0300

    setting the commits to pull in travis and fixing the benchmarks to include the other Mtac2 example

--- Benchmarks/make_all.sh
+++ Benchmarks/make_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for kind in Stats TypeClasses LtacPrimUncurry LtacVariants CanonicalStructures MTac2 Ltac2 OCaml TemplateCoq QuoteFlat Parametricity Printing; do
+for kind in Stats TypeClasses LtacPrimUncurry LtacVariants CanonicalStructures Mtac2 Mtac2Unfolded Ltac2 OCaml TemplateCoq QuoteFlat Parametricity Printing; do
     for speed in quick medium slow; do
 	cat > ${kind}_${speed}.v <<EOF
 Require Export Reify.Benchmarks.${kind}.
